6fb21f82e57fded806cde13c5e961642bb5370e6
Crowdbotics <team@crowdbotics.com>
2022-06-13 00:46:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "bc_window_washing_35691",
+  url: "https://bc_window_washing_35691.botics.co",
+  api: "https://bc_window_washing_35691.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
